87c19f50c2dbd36bdb1c3950c7a720a32826e2b3
Glaudiston Santos de Oliveira Neto <39042130+GlaudistonNeto@users.noreply.github.com>
2020-01-02 23:42:52 -0300

    blog contest and index screen added

--- App.js
+++ App.js
@@ -1,14 +1,24 @@
+import React from 'react';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
-import IndexScreen from './src/screens/indexScreen';
+import IndexScreen from './src/screens/IndexScreen'
+import { BlogProvider } from './src/context/BlogContext'
 
-const navigator = createStackNavigator({
+const navigator = createStackNavigator ({
   Index: IndexScreen
 }, {
-  initialRoutName: 'Index',
+  initialRouteName: 'Index',
   defaultNavigationOptions: {
-    title: 'Bogs'
+    title: 'Blogs'
   }
 });
 
-export default createAppContainer(navigator);
+const App = createAppContainer(navigator);
+
+export default () => {
+  return (
+    <BlogProvider>
+      <App />
+    </BlogProvider>
+  );
+};
